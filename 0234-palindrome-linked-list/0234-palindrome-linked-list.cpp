bc7ebe4c79303016dad085fcdfd2d01285a45757
Nakul Soni <nakulsoni2912@gmail.com>
2026-07-28 11:52:04 +0530

    Time: 7 ms (27.34%), Space: 126.7 MB (28.49%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* left;
+    bool check(ListNode* right) {
+        if(right == NULL)
+        return true;
+        bool ans = check(right->next);
+        if(ans == false)
+        return false;
+        if(left->val != right->val)
+        return false;
+        left = left->next;
+        return true;
+    }
+    bool isPalindrome(ListNode* head) {
+        left = head;
+        return check(head);
+    }
+};
